92bfd8da319343c3cc882cd5c5785d113043d07c
shivam24s <shivamshrivastav8055@gmail.com>
2026-01-27 14:02:52 +0530

    some changes added

---
+++ hosting/src/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyBvoxvEFv2Dvrul51aEK0Mdde6h_lp38",
+  authDomain: "hosting-check-263b9.firebaseapp.com",
+  projectId: "hosting-check-263b9",
+  storageBucket: "hosting-check-263b9.firebasestorage.app",
+  messagingSenderId: "996096092362",
+  appId: "1:996096092362:web:2409b0b5719e0859176c33",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
